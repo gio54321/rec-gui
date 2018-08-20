3a28b13ae8665a49bfefb9f1fd31d3208e1134fa
gio54321 <g.dellimmagine@gmail.com>
2018-08-20 17:10:14 +0200

    add auto repeat and elaboration script

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var BrowserWindow = require('electron').BrowserWindow;
 app.on("ready", function(){
   var mainWindow = new BrowserWindow({
     width:500,
-    height:600
+    height:700
   });
   mainWindow.loadURL("file://" + __dirname + "/index.html");
 })
--- js/main.js
+++ js/main.js
@@ -26,7 +26,11 @@ fs.readFile("default.conf.xml", "utf-8", function(err, data){
   document.getElementById("dig-gain").value = xmlDoc.getElementsByTagName("dig-gain")[0].childNodes[0].nodeValue;
   document.getElementById("filename").value = xmlDoc.getElementsByTagName("filename")[0].childNodes[0].nodeValue;
   document.getElementById("start-now").checked = (xmlDoc.getElementsByTagName("start-now")[0].childNodes[0].nodeValue == "true")? true : false;
-  document.getElementById("start-time").value = new Date().toLocaleString();
+  document.getElementById("repeat").checked = (xmlDoc.getElementsByTagName("repeat")[0].childNodes[0].nodeValue == "true")? true : false;
+  document.getElementById("launch-script").checked = (xmlDoc.getElementsByTagName("launch-script")[0].childNodes[0].nodeValue == "true")? true : false;
+  document.getElementById("processing-script").value = xmlDoc.getElementsByTagName("processing-script")[0].childNodes[0].nodeValue;
+
+  document.getElementById("start-time").value = new Date().toUTCString();
 
   recorderPath = xmlDoc.getElementsByTagName("recorder-path")[0].childNodes[0].nodeValue;
   if(recorderPath == "/"){
@@ -69,10 +73,37 @@ function controlData(){
   return true;
 }
 
+function launchRecording(){
+  var fftSize = document.getElementById("sample-rate").value / document.getElementById("sr-time-series").value;
+  var decimation = fftSize / document.getElementById("num-chan").value
+  var command = '--working-directory="'+ recorderPath + '" -x bash -c "sudo python2 PulsChan.py ' +
+    document.getElementById("acq-time").value + " " +
+    document.getElementById("sample-rate").value + " " +
+    document.getElementById("frequency").value + " " +
+    document.getElementById("rf-gain").value + " " +
+    document.getElementById("if-gain").value + " " +
+    document.getElementById("bb-gain").value + " " + fftSize + " " + decimation + " " +
+    document.getElementById("dig-gain").value + " " +
+    document.getElementById("filename").value +
+    " |& sudo tee " + recorderPath + "/REC_GUI/Last_log.txt" + '"';
+
+  exec('gnome-terminal ' + command, function(error, stdout, stderr){
+    console.log(stdout);
+    if(stderr)  alert("ERROR: " + stderr);
+    if (document.getElementById("launch-script").checked){
+      exec(document.getElementById("processing-script").value, function(error, stdout, stderr){
+        console.log(stdout);
+        if(stderr)  alert("ERROR: " + stderr);
+      });
+    }
+  });
+}
+
 function start(){
   if(countdownIsRunning){
     clearInterval(intervalId);
     document.getElementsByClassName("start-button")[0].value = "start";
+    document.getElementById("countdown").innerHTML = "00:00:00";
     countdownIsRunning = false;
   }else{
     if(!controlData()){
@@ -80,26 +111,19 @@ function start(){
     }
 
     if(document.getElementById("start-now").checked){
-      var fftSize = document.getElementById("sample-rate").value / document.getElementById("sr-time-series").value;
-      var decimation = fftSize / document.getElementById("num-chan").value
-      var command = '--working-directory="'+ recorderPath + '" -x bash -c "sudo python2 PulsChan.py ' +
-        document.getElementById("acq-time").value + " " +
-        document.getElementById("sample-rate").value + " " +
-        document.getElementById("frequency").value + " " +
-        document.getElementById("rf-gain").value + " " +
-        document.getElementById("if-gain").value + " " +
-        document.getElementById("bb-gain").value + " " + fftSize + " " + decimation + " " +
-        document.getElementById("dig-gain").value + " " +
-        document.getElementById("filename").value + 
-        " |& sudo tee " + recorderPath + "/REC_GUI/Last_log.txt" + '"';
-      exec('gnome-terminal ' + command, function(error, stdout, stderr){
-        console.log(stdout);
-        if(stderr)  alert("ERROR: " + stderr);
-      });
+      document.getElementById("start-time").value = new Date().toUTCString();
+      launchRecording();
+      if(document.getElementById("repeat").checked){
+        document.getElementById("start-time").value = new Date(new Date(document.getElementById("start-time").value).getTime() + 86164*1000).toUTCString();
+        document.getElementById("start-now").checked = false;
+        countdownSeconds = parseInt((Date.parse(document.getElementById("start-time").value) - Date.parse(new Date().toUTCString()))/1000);
+        intervalId = setInterval(countdown, 1000);
+        document.getElementsByClassName("start-button")[0].value = "stop";
+        countdownIsRunning = true;
+      }
     }
     else{
-      countdownSeconds = parseInt((Date.parse(document.getElementById("start-time").value) - Date.parse(new Date().toLocaleString()))/1000);
-      console.log(countdownSeconds);
+      countdownSeconds = parseInt((Date.parse(document.getElementById("start-time").value) - Date.parse(new Date().toUTCString()))/1000);
       if(countdownSeconds >= 0){
         intervalId = setInterval(countdown, 1000);
         document.getElementsByClassName("start-button")[0].value = "stop";
@@ -109,7 +133,6 @@ function start(){
       }
     }
   }
-
 }
 
 function countdown(){
@@ -118,22 +141,15 @@ function countdown(){
     document.getElementsByClassName("start-button")[0].value = "start";
     countdownIsRunning = false;
     if(controlData()){
-      var fftSize = document.getElementById("sample-rate").value / document.getElementById("sr-time-series").value;
-      var decimation = fftSize / document.getElementById("num-chan").value
-       var command = '--working-directory="'+ recorderPath + '" -x bash -c "sudo python2 PulsChan.py ' +
-        document.getElementById("acq-time").value + " " +
-        document.getElementById("sample-rate").value + " " +
-        document.getElementById("frequency").value + " " +
-        document.getElementById("rf-gain").value + " " +
-        document.getElementById("if-gain").value + " " +
-        document.getElementById("bb-gain").value + " " + fftSize + " " + decimation + " " +
-        document.getElementById("dig-gain").value + " " +
-        document.getElementById("filename").value + 
-        " |& sudo tee " + recorderPath + "/REC_GUI/Last_log.txt" + '"';
-      exec('gnome-terminal ' + command, function(error, stdout, stderr){
-        console.log(stdout);
-        if(stderr)  alert("ERROR: " + stderr);
-      });
+      launchRecording();
+      if(document.getElementById("repeat").checked){
+        document.getElementById("start-time").value = new Date(new Date(document.getElementById("start-time").value).getTime() + 86164*1000).toUTCString();
+        document.getElementById("start-now").checked = false;
+        countdownSeconds = parseInt((Date.parse(document.getElementById("start-time").value) - Date.parse(new Date().toUTCString()))/1000);
+        intervalId = setInterval(countdown, 1000);
+        document.getElementsByClassName("start-button")[0].value = "stop";
+        countdownIsRunning = true;
+      }
     }
   }
   if(!controlData()){
@@ -141,10 +157,11 @@ function countdown(){
     document.getElementsByClassName("start-button")[0].value = "start";
     countdownIsRunning = false;
   }
-  console.log(countdownSeconds);
+
   var hours = Math.floor(countdownSeconds / 3600);
   var minutes = Math.floor((countdownSeconds - (hours*3600))/60);
   var seconds = Math.floor((countdownSeconds -(hours*3600) -(minutes*60)));
   document.getElementById("countdown").innerHTML = hours + ":" + ("0" + minutes).slice(-2) + ":" + ("0" + seconds).slice(-2);
+
   countdownSeconds--;
 }
